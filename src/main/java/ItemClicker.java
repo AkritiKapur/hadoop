c4375f0a177e315fdbc18858f36a254ca9fc1366
AkritiKapur <akritikapur1@gmail.com>
2018-09-24 12:01:13 -0600

    top k logic added, sorting added - Amith and Akriti

--- src/main/java/ItemClicker.java
+++ src/main/java/ItemClicker.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -60,17 +61,94 @@ public class ItemClicker {
         }
     }
 
+    public static class SortMapper
+            extends Mapper<Object, Text, IntWritable, Text> {
+
+        public void map(Object key, Text value, Context context) throws  IOException, InterruptedException {
+
+            StringTokenizer str = new StringTokenizer(value.toString());
+            while(str.hasMoreTokens()) {
+                Text t0 = new Text(str.nextToken());
+                IntWritable t1 = new IntWritable(Integer.parseInt(str.nextToken()));
+                context.write(t1, t0);
+            }
+        }
+    }
+
+    public static  class SortReducer
+            extends Reducer<IntWritable, Text, Text, IntWritable> {
+
+        int mCount = 0;
+
+        // Source of finding top k - http://kamalnandan.com/hadoop/how-to-find-top-n-values-using-map-reduce/
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            mCount = 0;
+        }
+
+        public void reduce(IntWritable key, Iterable<Text> values, Context context)
+                throws  IOException, InterruptedException {
+
+            if(mCount < 10) {
+                for (Text t : values) {
+                    context.write(t, key);
+                    mCount ++;
+                }
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
+        Job job = Job.getInstance(conf, "item click frequency");
         job.setJarByClass(ItemClicker.class);
         job.setMapperClass(ItemClicker.TokenizerMapper.class);
         job.setCombinerClass(ItemClicker.IntSumReducer.class);
         job.setReducerClass(ItemClicker.IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path("clicks.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("/home/akriti/output2"));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        Path outputPath = new Path(args[1]);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, outputPath);
+        outputPath.getFileSystem(conf).delete(outputPath);
+        int jobStatus = job.waitForCompletion(true) ? 0:1;
+
+        if (jobStatus == 0) {
+            Configuration conf2 = new Configuration();
+            Job job2 = Job.getInstance(conf2);
+            job2.setJarByClass(ItemClicker.class);
+            job2.setMapperClass(ItemClicker.SortMapper.class);
+            job2.setReducerClass(ItemClicker.SortReducer.class);
+            // Sorts by frequency
+            job2.setSortComparatorClass(DescendingIntWritableComparable.DecreasingComparator.class);
+            job2.setNumReduceTasks(1);
+            job2.setOutputKeyClass(IntWritable.class);
+            job2.setOutputValueClass(Text.class);
+            Path outputPath1 = new Path(args[2]);
+            FileInputFormat.addInputPath(job2, outputPath);
+            FileOutputFormat.setOutputPath(job2, outputPath1);
+            outputPath1.getFileSystem(conf2).delete(outputPath1, true);
+            System.exit(job2.waitForCompletion(true)?0:1);
+        }
+
+
     }
+
+    // Source: https://stackoverflow.com/questions/18154686/how-to-implement-sort-in-hadoop
+    //this class is defined outside of main not inside
+    public static class DescendingIntWritableComparable extends IntWritable {
+        /** A decreasing Comparator optimized for IntWritable. */
+        public static class DecreasingComparator extends Comparator {
+            public int compare(WritableComparable a, WritableComparable b) {
+                return -super.compare(a, b);
+            }
+            public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+                return -super.compare(b1, s1, l1, b2, s2, l2);
+            }
+        }
+    }
+
+
 }
