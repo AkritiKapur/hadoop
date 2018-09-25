951de77d30f56c4eb4eb567246678c4fefda14ff
AkritiKapur <akritikapur1@gmail.com>
2018-09-24 23:04:39 -0600

    Sorting for PartC - Amith and Akriti

--- src/main/java/Success.java
+++ src/main/java/Success.java
@@ -7,22 +7,19 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.DataInput;
-import java.io.DataOutput;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.StringTokenizer;
+import java.util.Iterator;
 
 
 
 public class Success {
     public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, Transaction> {
+            extends Mapper<Object, Text, Text, Text> {
 
-
-        private final static IntWritable one = new IntWritable(1);
-        private final static IntWritable zero = new IntWritable(0);
-        private Transaction buyTransaction = new Transaction(one, zero);
-        private Transaction clickTransaction = new Transaction(zero, one);
         private Text word = new Text();
 
         //@Override
@@ -34,12 +31,11 @@ public class Success {
                 String line = word.toString();
                 Text itemID = new Text(getItemID(line));
                 if (isBuy(line)) {
-                    context.write(itemID, buyTransaction);
+                    context.write(itemID, new Text("1:0"));
                 }
                 else {
-                    context.write(itemID, clickTransaction);
+                    context.write(itemID, new Text("0:1"));
                 }
-
             }
         }
 
@@ -55,27 +51,21 @@ public class Success {
     }
 
     public static class IntSumReducer
-            extends Reducer<Text, Transaction, Text, FloatWritable> {
+            extends Reducer<Text, Text, Text, FloatWritable> {
         private FloatWritable result = new FloatWritable();
 
 
-        IntSumReducer() {
-            System.out.println("in reducer");
-        }
-
-
         //@Override
-        public void reduce(Text key, Iterable<Transaction> values,
+        public void reduce(Text key, Iterable<Text> values,
                            Context context
         ) throws IOException, InterruptedException {
-            System.out.println("reduce func");
             int buy = 0;
             int click = 0;
-            for (Transaction val : values) {
-                buy += val.getBuy().get();
-                click += val.getClick().get();
+            for (Text val : values) {
+                String[] transaction = val.toString().split(":");
+                buy += Integer.parseInt(transaction[0]);
+                click += Integer.parseInt(transaction[1]);
             }
-            System.out.println("dsfkjsfd");
             if (click != 0) {
                 result.set((float) buy / (float) click);
             } else {
@@ -86,70 +76,82 @@ public class Success {
     }
 
 
-    public static class Transaction implements Writable {
-        private IntWritable buy;
-        private IntWritable click;
-
-        Transaction(IntWritable buy, IntWritable click) {
-            this.buy = buy;
-            this.click = click;
-        }
-
-        public IntWritable getBuy() {
-            return buy;
-        }
-
-        public IntWritable getClick() {
-            return click;
-        }
-
-        @Override
-        public void write(DataOutput dataOutput) throws IOException {
-            buy.write(dataOutput);
-            click.write(dataOutput);
-        }
-
-        @Override
-        public void readFields(DataInput dataInput) throws IOException {
-            buy.readFields(dataInput);
-            click.readFields(dataInput);
-        }
-    }
-
     public static class SortMapper
-            extends Mapper<Object, Text, IntWritable, Text> {
+            extends Mapper<Object, Text, FloatWritable, LongWritable> {
 
         public void map(Object key, Text value, Context context) throws  IOException, InterruptedException {
 
             StringTokenizer str = new StringTokenizer(value.toString());
             while(str.hasMoreTokens()) {
-                Text t0 = new Text(str.nextToken());
-                IntWritable t1 = new IntWritable(Integer.parseInt(str.nextToken()));
+                LongWritable t0 = new LongWritable(Long.parseLong(str.nextToken()));
+                FloatWritable t1 = new FloatWritable(Float.parseFloat(str.nextToken()));
                 context.write(t1, t0);
             }
         }
     }
 
     public static  class SortReducer
-            extends Reducer<IntWritable, Text, Text, IntWritable> {
+            extends Reducer<FloatWritable, LongWritable, LongWritable, FloatWritable> {
 
-        int mCount = 0;
+        int mCount = 1;
 
         // Source of finding top k - http://kamalnandan.com/hadoop/how-to-find-top-n-values-using-map-reduce/
         @Override
         protected void setup(Context context) throws IOException, InterruptedException {
-            mCount = 0;
+            mCount = 1;
         }
 
-        public void reduce(IntWritable key, Iterable<Text> values, Context context)
+        public static Comparator<LongWritable> LongWComparator = new Comparator<LongWritable>() {
+
+
+            public int compare(LongWritable l1, LongWritable l2) {
+                //String StudentName1 = s1.getName().toUpperCase();
+                //String StudentName2 = s2.getName().toUpperCase();
+
+                //ascending order
+                return l2.compareTo(l1);
+
+                //descending order
+                //return StudentName2.compareTo(StudentName1);
+            }
+        };
+
+        public void reduce(FloatWritable key, Iterable<LongWritable> values, Context context)
                 throws  IOException, InterruptedException {
 
-            if(mCount < 10) {
-                for (Text t : values) {
-                    context.write(t, key);
-                    mCount ++;
-                }
+//            if(mCount > 10) {
+//                for (Text t : values) {
+//                    context.write(t, key);
+//                    mCount ++;
+//                }
+//            }
+
+            System.out.println(key);
+            ArrayList<LongWritable>  lwl = new ArrayList<LongWritable>();
+            Iterator<LongWritable> iter = values.iterator();
+            while(iter.hasNext()) {
+                //LongWritable lw = iter.next();
+                //System.out.println();
+                lwl.add(new LongWritable(iter.next().get()));
             }
+
+
+            for(LongWritable l: lwl) {
+                System.out.println(l);
+                //System.out.println(val);
+            }
+
+            Collections.sort(lwl);
+
+
+            for(LongWritable l: lwl) {
+                if (mCount >10) {
+                    break;
+                }
+                //System.out.println(l);
+                context.write(l, key);
+            }
+
         }
     }
 
@@ -160,7 +162,7 @@ public class Success {
 
         job.setNumReduceTasks(1);
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Transaction.class);
+        job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(FloatWritable.class);
         job.setMapperClass(Success.TokenizerMapper.class);
@@ -175,32 +177,34 @@ public class Success {
 
         FileOutputFormat.setOutputPath(job, outputPath);
         outputPath.getFileSystem(conf).delete(outputPath);
-        System.exit(job.waitForCompletion(true) ? 0:1);
+        int jobStatus = job.waitForCompletion(true) ? 0:1;
 
-//        if (jobStatus == 0) {
-//            Configuration conf2 = new Configuration();
-//            Job job2 = Job.getInstance(conf2);
-//            job2.setJarByClass(ItemClicker.class);
-//            job2.setMapperClass(ItemClicker.SortMapper.class);
-//            job2.setReducerClass(ItemClicker.SortReducer.class);
-//            // Sorts by frequency
-//            job2.setSortComparatorClass(ItemClicker.DescendingIntWritableComparable.DecreasingComparator.class);
-//            job2.setNumReduceTasks(1);
-//            job2.setOutputKeyClass(IntWritable.class);
-//            job2.setOutputValueClass(Text.class);
-//            Path outputPath1 = new Path(args[2]);
-//            FileInputFormat.addInputPath(job2, outputPath);
-//            FileOutputFormat.setOutputPath(job2, outputPath1);
-//            outputPath1.getFileSystem(conf2).delete(outputPath1, true);
-//            System.exit(job2.waitForCompletion(true)?0:1);
-//        }
+        if (jobStatus == 0) {
+            Configuration conf2 = new Configuration();
+            Job job2 = Job.getInstance(conf2);
+            job2.setJarByClass(Success.class);
+            job2.setMapperClass(Success.SortMapper.class);
+            job2.setReducerClass(Success.SortReducer.class);
+            // Sorts by frequency
+            job2.setSortComparatorClass(Success.DescendingFloatWritableComparable.DecreasingComparator.class);
+            job2.setMapOutputValueClass(LongWritable.class);
+            job2.setMapOutputKeyClass(FloatWritable.class);
+            job2.setNumReduceTasks(1);
+            job2.setOutputKeyClass(LongWritable.class);
+            job2.setOutputValueClass(FloatWritable.class);
+            Path outputPath1 = new Path("/home/akriti/success=final");
+            FileInputFormat.addInputPath(job2, outputPath);
+            FileOutputFormat.setOutputPath(job2, outputPath1);
+            outputPath1.getFileSystem(conf2).delete(outputPath1, true);
+            System.exit(job2.waitForCompletion(true)?0:1);
+        }
 
 
     }
 
     // Source: https://stackoverflow.com/questions/18154686/how-to-implement-sort-in-hadoop
     //this class is defined outside of main not inside
-    public static class DescendingIntWritableComparable extends IntWritable {
+    public static class DescendingFloatWritableComparable extends FloatWritable {
         /** A decreasing Comparator optimized for IntWritable. */
         public static class DecreasingComparator extends Comparator {
             public int compare(WritableComparable a, WritableComparable b) {
